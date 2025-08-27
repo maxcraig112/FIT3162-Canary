3b02b9a3f1eb69e1f8e2c3ece824afe2ac037d6c
mcra0009 <mcra0009@student.monash.edu>
2025-08-27 19:14:58 +1000

    update email regex https://www.heybounce.io/email-validation-regex

--- src/services/golang-auth-service/api/util.go
+++ src/services/golang-auth-service/api/util.go
@@ -7,7 +7,7 @@ import (
 
 // Helpers
 func isValidEmail(email string) bool {
-	re := regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`)
+	re := regexp.MustCompile(`^(?=.{1,254}$)(?=.{1,64}@)[A-Za-z0-9!#$%&'*+/=?^_{|}~-]+(?:\\.[A-Za-z0-9!#$%&'*+/=?^_{|}~-]+)*@(?:(?:[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?\\.)+[A-Za-z]{2,}|\\[(?:IPv6:[A-F0-9]{0,4}(?::[A-F0-9]{0,4}){2,7}|(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))\\]$`)
 	return re.MatchString(email)
 }
 
